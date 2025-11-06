2cca03312093811b8a96297a7ebf2482cddc1f4d
Geokobby <geokobbymail@gmail.com>
2025-11-06 22:23:53 +0000

    created a new ChatMessages component to handle the chats

--- chatbot/src/App.jsx
+++ chatbot/src/App.jsx
@@ -1,49 +1,14 @@
 import React from 'react'
 import Textbar from './components/textBar'
-import Chats from './components/Chats'
+import ChatMessages from './components/ChatMessages'
 
 function App() {
     
-    const chatMessages =[{message:"Hello Bot", 
-            sender:"user",
-             key:"1"}, 
-          { message:"Hello how can I help you?", 
-            sender:"bot",
-            key:"2"},
-          {message:"What is today",
-             sender: "user",
-             key:"3"},         
-          { message: "Today is Tuesday November 4",
-             sender: "bot",
-             key:"4"}];
-
-            //  this is another way to do the map function and uses the {chatMessageComponent} variable return component in the JSX
-            // const chatMessageComponent = chatMessages.map((chatMesage)=>{
-            // return <Chats 
-            // message={chatMesage.message} 
-            // sender = {chatMesage.sender}
-            // key ={chatMesage.key}/>
-            // })
-
+   
   return (
     <div>
-    <Textbar />
-            {/* <Chats message="Hello Bot" sender="user" />
-            <Chats message="Hello how can I help you?" sender="bot" />
-            <Chats message="What is today" sender="user" />
-            <Chats message="Today is Tuesday November 4 " sender="bot" />  */}  
-
-             {/*  This is a test of the chatMessageComponent variable from the map comment before*/}
-            {/* {chatMessageComponent}  */}
-
-              {/* This is the map function directly in the JSX */}
-              {chatMessages.map((chatMesage)=>{
-            return <Chats 
-            message={chatMesage.message} 
-            sender = {chatMesage.sender}
-            key ={chatMesage.key}/>
-            })} 
-
+      <Textbar />
+      <ChatMessages />
     </div>
   )
 }
