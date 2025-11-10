1f732c8b14ce13e85ab39e025d017da5c09fa66d
Geokobby <geokobbymail@gmail.com>
2025-11-10 12:04:01 +0000

    Loading input from inputTextBox

--- chatbot/src/components/textBar.jsx
+++ chatbot/src/components/textBar.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import { useState } from 'react';
 
 function Textbar() {
+  // State to hold the input text value
+  const [inputText, setInputText] = useState("");
+  function saveInputText(event){
+    setInputText(event.target.value);
+  }
+// Function to handle sending the message
+  function sendMessage(){
+    console.log(inputText)
+  }
   return (
     <div>
-      <input type="text" placeholder="Type your message..." />
-      <button>Send</button>
+      {/* Input box to type message, onChange text the vaule of the inputText */}
+      <input type="text" placeholder="Type your message..."
+      onChange={saveInputText} />
+
+      {/* OnClick to add text from the inputText box */}
+      <button
+      onClick={sendMessage }>Send</button>
     </div>
   )
 }
